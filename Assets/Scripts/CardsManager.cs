e24a71561439d3a1f48ccb2645f04c336dc066e7
Paul13517 <pavelfsn@yandex.ru>
2022-12-10 15:57:21 +0300

    Dropdown,UI

--- Assets/Scripts/CardsManager.cs
+++ Assets/Scripts/CardsManager.cs
@@ -11,6 +11,130 @@ public class CardsManager : MonoBehaviour
 public GameObject[] cardsBack;
 public GameObject[] cards;
 
+public Image[] images;
+private Tween tween;
+
+public LoadImage[] lI;
+
+
+      IEnumerator Start()
+    {
+      for(int i = 0; i < 4; i++){
+       
+        tween = cards[i].transform.DORotate(new Vector3(0,-90,0), 0.9f);
+           lI[i].StartLoading();
+            yield return new WaitForSeconds(0.1f);
+         } 
+         
+        
+        for(int i = 0; i < cards.Length; i++){
+          yield return new WaitForSeconds(0.1f);
+        cardsBack[i].SetActive(true);
+        images[i].enabled = true;
+        }
+    }
+
+void Update(){
+    if(Input.GetMouseButtonDown(0)){
+       
+      
+       StartCoroutine(CardsPositionBack());
+    }
+  }
+       
+    IEnumerator CardsPositionBack(){
+     for(int i = 0; i < 4; i++){
+      tween = cardsBack[i].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+      tween = images[i].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+     }
+      yield return new WaitForSeconds(1f);
+      for(int i = 0; i < 4; i++){
+       tween = cards[i].transform.DORotate(new Vector3(0,0,0), 0.1f); 
+      }
+  }         
+}
+
+/*
+-- Поочередный режим
+public GameObject[] cardsBack;
+public GameObject[] cards;
+
+public Image[] images;
+private Tween tween;
+
+public LoadImage[] lI;
+
+
+      IEnumerator Start()
+    {
+      for(int i = 0; i < 4; i++){
+       
+        tween = cards[i].transform.DORotate(new Vector3(0,-90,0), 0.9f);
+           lI[i].StartLoading();
+            yield return new WaitForSeconds(0.1f);
+         } 
+         
+        
+        for(int i = 0; i < cards.Length; i++){
+          yield return new WaitForSeconds(0.1f);
+        cardsBack[i].SetActive(true);
+        images[i].enabled = true;
+        }
+    }
+
+
+
+
+-- Дотвин. Повороты одновременно //доработанная загрузка
+
+public GameObject[] cardsBack;
+public GameObject[] cards;
+
+public Image[] images;
+private Tween tween;
+
+public LoadImage[] lI;
+
+
+      IEnumerator Start()
+    {
+      for(int i = 0; i < 4; i++){
+        tween = cards[i].transform.DORotate(new Vector3(0,-90,0), 0.9f);
+           lI[i].StartLoading();
+         } 
+         
+        yield return new WaitForSeconds(1f);
+        for(int i = 0; i < cards.Length; i++){
+        cardsBack[i].SetActive(true);
+        images[i].enabled = true;
+        }
+    }
+
+void Update(){
+    if(Input.GetMouseButtonDown(0)){
+       
+      
+       StartCoroutine(CardsPositionBack());
+    }
+  }
+       
+    IEnumerator CardsPositionBack(){ // поворот в изначальную позицию
+     for(int i = 0; i < 4; i++){
+      tween = cardsBack[i].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+      tween = images[i].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+     }
+      yield return new WaitForSeconds(1f);
+      for(int i = 0; i < 4; i++){
+       tween = cards[i].transform.DORotate(new Vector3(0,0,0), 0.1f); 
+      }
+  } 
+
+
+
+-- Дотвин. Повороты одновременно //доработать загрузку
+public GameObject[] cardsBack;
+public GameObject[] cards;
+
 public LoadImage lI;
 private Tween tween;
 
@@ -39,17 +163,18 @@ void Update(){
   }
        
     IEnumerator CardsPositionBack(){
-    
-      tween = cardsBack[0].transform.DORotate(new Vector3(0,90,0), 0.9f); 
-      tween = lI.img[0].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+     for(int i = 0; i < 4; i++){
+      tween = cardsBack[i].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+      tween = lI.img[i].transform.DORotate(new Vector3(0,90,0), 0.9f); 
+     }
       yield return new WaitForSeconds(1f);
-       tween = cards[0].transform.DORotate(new Vector3(0,0,0), 0.1f); 
-    
-  }   
+      for(int i = 0; i < 4; i++){
+       tween = cards[i].transform.DORotate(new Vector3(0,0,0), 0.1f); 
+      }
+  }  
 
-       
-      
-}
+
+*/
 
 
 
--- Assets/Scripts/LoadImage.cs
+++ Assets/Scripts/LoadImage.cs
@@ -7,40 +7,44 @@ using DG.Tweening;
 public class LoadImage : MonoBehaviour
 {
    [SerializeField] private string url;
+   private Image img;
 
-    public Image[] img;
+    //public Image[] img;
     public bool isLoaded = false;
     
 
     public void StartLoading()
+
     {
-         StartCoroutine(GetImage());
+       img = GetComponent<Image>();
+       StartCoroutine(GetImage());
        
     }
 
   
     IEnumerator GetImage()
     {
-        
-        for(int i = 0; i <  img.Length; i++){
+         
+       
         UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url);
-        
+         
         yield return webRequest.SendWebRequest();
-
+         
         if(webRequest.isDone == false){
             Debug.Log(webRequest.error);
         }
         else{
             Texture texture = ((DownloadHandlerTexture)webRequest.downloadHandler).texture;
-           
-            img[i].sprite = Sprite.Create((Texture2D)texture, new Rect (0,0,texture.width, texture.height), Vector2. zero);
+         
             
+            img.sprite = Sprite.Create((Texture2D)texture, new Rect (0,0,texture.width, texture.height), Vector2. zero);
+
+          }
            
            
-           // yield return new WaitForSeconds(0.03f);
-             // lI.img[i].enabled = true; перенести в CardsManager
-            }
-        }
+
+            
+        
         
     }
 
